f436a46668931fb5e26e398cbbdcd36bf617a4f9
Luyanda Junior <youtunechannel03@gmail.com>
2024-07-27 12:35:06 +0200

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python
+
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the page to scrape
-url = 'https://www.example.com/local-weather'
+def main():
+    # URL of the page to scrape
+    url = 'https://www.example.com/local-weather'
 
-# Send a GET request to the page
-response = requests.get(url)
+    # Send a GET request to the page
+    response = requests.get(url)
 
-# Parse the page content with BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+    # Parse the page content with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the desired information
-weather = soup.find('div', class_='weather-forecast').text
+    # Extract the desired information
+    weather = soup.find('div', class_='weather-forecast').text
 
-print(f'Local Weather: {weather}')
+    print(f'Local Weather: {weather}')
+
+if __name__ == '__main__':
+    main()
